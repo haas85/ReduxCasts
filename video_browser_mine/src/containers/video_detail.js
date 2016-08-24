66b030e0b92377f89f9555adaf50a69d3154817c
Inigo Gonzalez Vazquez <ingonza85@gmail.com>
2016-08-24 12:50:52 +0200

    Video details working

--- video_browser_mine/src/containers/video_detail.js
+++ video_browser_mine/src/containers/video_detail.js
@@ -6,6 +6,7 @@ class VideoDetail extends Component {
 
   constructor(props){
     super(props);
+      window.cosa = this;
     this.comments= props.comments;
   }
 
--- video_browser_mine/src/reducers/selected_video_reducer.js
+++ video_browser_mine/src/reducers/selected_video_reducer.js
@@ -3,9 +3,10 @@ import { SELECT_VIDEO } from '../actions/videoManager';
 export default function (state = {videoHistory: []}, action) {
     switch (action.type) {
         case SELECT_VIDEO:
-            state.selectedVideo = action.payload;
-            state.videoHistory.push(action.payload);
-            return state;
+            return Object.assign({}, {
+                video: action.payload,
+                videoHistory: [...state.videoHistory, action.payload]
+            });
         default:
             return state;
     }
