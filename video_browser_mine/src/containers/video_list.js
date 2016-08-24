a8baf0049604815241d7c036f7076019fa9ba664
Inigo Gonzalez Vazquez <ingonza85@gmail.com>
2016-08-24 13:43:32 +0200

    video list to class

--- video_browser_mine/src/containers/video_list.js
+++ video_browser_mine/src/containers/video_list.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import VideoListItem from '../containers/video_list_item';
+import { selectVideo } from '../actions/videoManager';
 
-const VideoList = (props) => {
-  const videoItems = props.videoList.map((video) => {
+class VideoList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const videoItems = this.props.videoList.map((video) => {
+      return (
+          <VideoListItem
+              key={video.etag}
+              video={video} />
+      );
+    });
+
     return (
-      <VideoListItem
-        onVideoSelect={props.onVideoSelect}
-        key={video.etag}
-        video={video} />
+        <ul className="col-md-4 list-group">
+          {videoItems}
+        </ul>
     );
-  });
-
-  return (
-    <ul className="col-md-4 list-group">
-      {videoItems}
-    </ul>
-  );
+  }
 };
 
 function mapStateToProps(state) {
   return {
-    videoList: state.videoList
+    videoList: state.videoList,
+    videoSelected: state.videoSelected
   };
 }
 
-export default connect(mapStateToProps, null)(VideoList);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ selectVideo: selectVideo }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(VideoList);
