53127e625a341569ef4fd946f3e65f284ef901dc
Inigo Gonzalez Vazquez <ingonza85@gmail.com>
2016-08-28 19:10:06 +0200

    Removed react-autocomplete

--- video_browser_mine/src/containers/search_bar.js
+++ video_browser_mine/src/containers/search_bar.js
@@ -2,26 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchVideos } from '../actions/videoManager';
-import Autocomplete from 'react-autocomplete';
-
-function matchStateToTerm (state, value) {
-  return (
-      state.name.toLowerCase().indexOf(value.toLowerCase()) !== -1 ||
-      state.abbr.toLowerCase().indexOf(value.toLowerCase()) !== -1
-  )
-};
-
-function sortStates (a, b, value) {
-  return (
-      a.name.toLowerCase().indexOf(value.toLowerCase()) >
-      b.name.toLowerCase().indexOf(value.toLowerCase()) ? 1 : -1
-  )
-};
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { term: this.props.term || 'surfboard' };
+    let term = this.props.term || 'surfboard';
+    this.state = { term: term, history: [term] };
     this.props.fetchVideos(this.state.term);
   }
 
@@ -33,32 +19,15 @@ class SearchBar extends Component {
               onChange={event => this.onInputChange(event.target.value)} />
         </div>
     );
-    return (
-      <div className="search-bar">
-        <Autocomplete
-            value={this.state.term}
-            inputProps={{name: "Search Term", id: "video-term"}}
-            items={this.props.videoSelected.history}
-            getItemValue={(item) => item.name}
-            shouldItemRender={matchStateToTerm}
-            sortItems={sortStates}
-            onChange={event => this.onInputChange(event.target.value)}
-            onSelect={value => this.onInputChange(value)}
-            renderItem={(item, isHighlighted) => (
-                <div
-                    style={isHighlighted ? styles.highlightedItem : styles.item}
-                    key={item.abbr}
-                >{item.name}</div>
-            )}
-        />
-      </div>
-    );
   }
 
   onInputChange(term) {
     // If we want to notify a change to an upper view without using redux we use data down actions up, so we need
     // to receive the function that receives data as a parameter
-    this.setState({term});
+    this.setState({
+      term: term,
+      history: [ ...this.state.history, term]
+    });
     this.props.fetchVideos(term);
   }
 }
