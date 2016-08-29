5c56cbc6ccb02030687dcafc7580719affed84fa
Inigo Gonzalez Vazquez <ingonza85@gmail.com>
2016-08-29 19:43:18 +0200

    Changed structure

---
+++ video_browser_mine/src/components/app.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from '../containers/search_bar';
+import VideoList from '../containers/video_list';
+import VideoDetail from '../containers/video_detail';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoDetail/>
+                <VideoList />
+            </div>
+        );
+    }
+}
--- video_browser_mine/src/index.js
+++ video_browser_mine/src/index.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import SearchBar from './containers/search_bar';
-import VideoList from './containers/video_list';
-import VideoDetail from './containers/video_detail';
+import App from './components/app';
 
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
@@ -24,44 +22,9 @@ ActorHandler(_store);
 
 export const store = _store;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos: [],
-      comments: [],
-      selectedVideo: null,
-      term: 'surfboards'
-    };
-  }
-
-  videoComments(video) {
-    if (!video && !this.state.selectedVideo) {
-      return
-    }
-
-    let _video = video || this.state.selectedVideo;
-  }
-
-  selectedVideo(selectedVideo) {
-      this.setState({selectedVideo});
-      this.videoComments(selectedVideo);
-  }
-
-  render() {
-    // const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
-
-    return (
-        <Provider store={_store}>
-            <div>
-                <SearchBar/>
-                <VideoDetail comments={this.state.comments}/>
-                <VideoList />
-            </div>
-        </Provider>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+    <Provider store={_store}>
+        <App />
+    </Provider>,
+    document.querySelector('.container')
+);
